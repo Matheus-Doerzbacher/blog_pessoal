41a26a39a4571e95156120735ded207b0c3d698c
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-09-09 22:15:03 -0300

    falta tela de login

---
+++ src/app/(privado)/(home)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Olá Mundo</h1>
+      <p>Bem-vindo à página inicial!</p>
+    </div>
+  )
+}
--- src/app/(privado)/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function HomePage() {
-  const nomeVariavel = 'teste 3'
-
-  return <h1>{nomeVariavel}</h1>
-}
